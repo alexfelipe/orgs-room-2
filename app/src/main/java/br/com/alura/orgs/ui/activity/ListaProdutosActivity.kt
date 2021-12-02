25f9f76bfa04df0af554e636b74089aa671e9064
Alex Felipe <alexfelipevieira@gmail.com>
2021-12-02 13:22:38 -0300

    3.2

--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -2,10 +2,16 @@ package br.com.alura.orgs.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.lifecycleScope
+import br.com.alura.orgs.R
 import br.com.alura.orgs.database.AppDatabase
 import br.com.alura.orgs.databinding.ActivityListaProdutosActivityBinding
+import br.com.alura.orgs.extensions.vaiPara
+import br.com.alura.orgs.model.Usuario
 import br.com.alura.orgs.preferences.dataStore
 import br.com.alura.orgs.preferences.usuarioLogadoPreferences
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
@@ -27,7 +33,6 @@ class ListaProdutosActivity : AppCompatActivity() {
         AppDatabase.instancia(this).usuarioDao()
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -43,18 +48,45 @@ class ListaProdutosActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_lista_produtos, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_lista_produtos_sair_do_app -> {
+                desloga()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun desloga() {
+        lifecycleScope.launch {
+            dataStore.edit { preferences ->
+                preferences.remove(usuarioLogadoPreferences)
+            }
+        }
+    }
+
     private suspend fun verificaUsuarioLogado() {
         dataStore.data.collect { preferences ->
             preferences[usuarioLogadoPreferences]?.let { usuarioId ->
                 buscaUsuario(usuarioId)
-            } ?: finish()
+            } ?: vaiParaLogin()
         }
     }
 
-    private suspend fun buscaUsuario(usuarioId: String) {
-        usuarioDao.buscaPorId(usuarioId)?.let { usuarioEncontrado ->
+    private suspend fun buscaUsuario(usuarioId: String): Usuario? {
+        return usuarioDao.buscaPorId(usuarioId)?.also { usuarioEncontrado ->
             title = usuarioEncontrado.nome
-        } ?: finish()
+        }
+    }
+
+    private fun vaiParaLogin() {
+        vaiPara(LoginActivity::class.java)
+        finish()
     }
 
     private suspend fun buscaProdutos() {
